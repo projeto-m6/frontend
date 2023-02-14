50e273fb2beb3e4fe7709ebf33e14ebd8ab704bc
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-14 18:39:27 -0400

    feat: masks

---
+++ src/utils/masks.ts
@@ -0,0 +1,23 @@
+export const cpfMask = (value: string) => {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export const cepMask = (value: string) => {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{5})(\d{1,3})/, '$1-$2')
+    .replace(/(-\d{3})\d+?$/, '$1');
+};
+
+export const phoneMask = (value: string) => {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d{1,4})/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1');
+};
