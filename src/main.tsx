db8e91a306633aff607a5e9c2f097f11c9d578b5
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-15 10:35:01 -0400

    feat: add theme styled components

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { globalStyles } from './styles/global';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      {globalStyles()}
-      <App />
+      <ThemeProvider theme={theme}>
+        {globalStyles()}
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
