e2cf4aad74626b0b6d09af05e968f3c73658e7c2
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-15 10:18:49 -0400

    feat: register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,12 +1,194 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Input } from '../../components/Input';
+import { InputRadio } from '../../components/InputRadio';
 import { B2500, H5500 } from '../../styles/typography';
+import { cepMask, cpfMask, phoneMask } from '../../utils/masks';
+import { Button, Container, Form } from './styles';
+import { userRegister } from '../../validators/userRegister';
+import { ErrorMessage } from '../../components/Input/styles';
+
+interface IUserRegisterForm {
+  name: string;
+  email: string;
+  cpf: string;
+  cell: string;
+  birthdate: string;
+  description: string;
+  password: string;
+  confirmPassword: string;
+  is_buyer: string;
+  cep: string;
+  state: string;
+  city: string;
+  road: string;
+  number: string;
+  complement: string;
+}
 
 export const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IUserRegisterForm>({
+    resolver: yupResolver(userRegister),
+  });
+
+  const [cpf, setCpf] = useState('');
+  const [cep, setCep] = useState('');
+  const [phone, setPhone] = useState('');
+
+  console.log(errors);
+
+  const onSubmit = (data: IUserRegisterForm) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <form>
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <H5500>Cadastro</H5500>
         <B2500>Informações pessoais</B2500>
-      </form>
-    </div>
+        <Input
+          label="Nome"
+          placeholder="Ex: Samuel Leão"
+          name="name"
+          register={register}
+          errors={errors}
+        />
+        <Input
+          label="Email"
+          placeholder="Ex: samuel@kenzie.com.br"
+          name="email"
+          register={register}
+          errors={errors}
+        />
+        <Input
+          label="CPF"
+          placeholder="000.000.000-00"
+          name="cpf"
+          register={register}
+          errors={errors}
+          value={cpf}
+          rules={{
+            onChange: (e) => setCpf(cpfMask(e.target.value)),
+          }}
+        />
+        <Input
+          label="Celular"
+          placeholder="(DDD) 90000-0000"
+          name="cell"
+          register={register}
+          errors={errors}
+          value={phone}
+          rules={{
+            onChange: (e) => setPhone(phoneMask(e.target.value)),
+          }}
+        />
+        <Input
+          label="Data de Nascimento"
+          type="date"
+          name="birthdate"
+          register={register}
+          errors={errors}
+        />
+        <Input
+          label="Descrição"
+          placeholder="Digitar descrição"
+          name="description"
+          register={register}
+          errors={errors}
+        />
+        <B2500>Informações de endereço</B2500>
+        <Input
+          label="CEP"
+          placeholder="00000-000"
+          name="cep"
+          register={register}
+          errors={errors}
+          value={cep}
+          rules={{
+            onChange: (e) => setCep(cepMask(e.target.value)),
+          }}
+        />
+        <div className="flex">
+          <Input
+            label="Estado"
+            placeholder="Digitar Estado"
+            name="state"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            label="Cidade"
+            placeholder="Digitar Cidade"
+            name="city"
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <Input
+          label="Rua"
+          placeholder="Digitar rua"
+          name="road"
+          register={register}
+          errors={errors}
+        />
+        <div className="flex">
+          <Input
+            label="Numero"
+            placeholder="Digitar numero"
+            name="number"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            label="Complemento"
+            placeholder="Ex: apart 307"
+            name="complement"
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <B2500>Tipo de conta</B2500>
+        <div className="flex">
+          <InputRadio
+            id="comprador"
+            label="Comprador"
+            name="is_buyer"
+            value="true"
+            register={register}
+          />
+          <InputRadio
+            id="anunciante"
+            label="Anunciante"
+            name="is_buyer"
+            value="false"
+            register={register}
+          />
+        </div>
+        {errors.is_buyer && <ErrorMessage>Escolha uma das opções acima</ErrorMessage>}
+
+        <Input
+          label="Senha"
+          type="password"
+          placeholder="Digitar senha"
+          name="password"
+          register={register}
+          errors={errors}
+        />
+        <Input
+          label="Confirmar senha"
+          type="password"
+          placeholder="confirmar senha"
+          name="confirmPassword"
+          register={register}
+          errors={errors}
+        />
+        <Button type="submit">Finalizar Cadastro</Button>
+      </Form>
+    </Container>
   );
 };
