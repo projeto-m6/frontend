5e3ae87735b81313cf96f1b60de7cb8542c4839f
Gabriel <gabriellf.dev@gmail.com>
2023-02-14 23:57:36 -0300

    create login page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,12 +1,35 @@
-import { B2500, H5500 } from '../../styles/typography';
+import { useForm } from "react-hook-form";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { B1600, B2500, H5500 } from "../../styles/typography";
+import * as S from "./styles";
 
 export const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
+
   return (
-    <div>
-      <form>
-        <H5500>Cadastro</H5500>
-        <B2500>Informações pessoais</B2500>
-      </form>
-    </div>
+    <>
+      <Header />
+      <S.Container>
+        <S.ContainerLogin>
+          <H5500>Login</H5500>
+
+          <form>
+            <Input label="Usuario" name="user" register={register} />
+            <Input label="Password" name="password" register={register} />
+
+            <button>Entrar</button>
+          </form>
+          <div className="containerRegister">
+            <B1600>Ainda nao tem uma conta?</B1600>
+            <button><B2500>Cadastrar</B2500></button>
+          </div>
+        </S.ContainerLogin>
+      </S.Container>
+    </>
   );
 };
