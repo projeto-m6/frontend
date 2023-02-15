80e19f058c23f462333d2f94057c2846f71ccc51
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-15 10:17:33 -0400

    feat: user register validator

--- src/validators/userRegister.ts
+++ src/validators/userRegister.ts
@@ -9,17 +9,15 @@ export const userRegister = yup.object().shape({
     .min(14, 'Minimo de 11 digitos')
     .max(14, 'Máximo de 11 digitos'),
   cell: yup.string().required('Contato obrigatório'),
-  bithdate: yup.string().required('Data de nascimento obrigatório'),
+  birthdate: yup.string().required('Data de nascimento obrigatório'),
   description: yup.string().required('Descrição obrigatório'),
-  address: yup.object({
-    cep: yup.string().required('Cep obrigatório'),
-    state: yup.string().required('Estado obrigatório'),
-    city: yup.string().required('Cidade Obrigatória'),
-    road: yup.string().required('Rua obrigatorio'),
-    number: yup.string().required('Número obrigatorio'),
-    complemento: yup.string().notRequired(),
-  }),
-  is_buyer: yup.string().required('Tipo de Conta obrigatório'),
+  cep: yup.string().required('Cep obrigatório'),
+  state: yup.string().required('Estado obrigatório'),
+  city: yup.string().required('Cidade Obrigatória'),
+  road: yup.string().required('Rua obrigatorio'),
+  number: yup.string().required('Número obrigatorio'),
+  complemento: yup.string().notRequired(),
+  is_buyer: yup.boolean().required('Tipo de Conta obrigatório'),
   password: yup.string().required('Senha obrigatório'),
   confirmPassword: yup
     .string()
