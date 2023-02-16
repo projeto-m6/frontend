f6a024a3befd676def0768d963accedf9995cc0f
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-02-16 14:48:56 -0300

    Feat-ModalCriacaoAnuncio - Added Validations

---
+++ src/components/ModalCreateAnnouncement/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
+    background-color: '$grey10';
+    max-width: 25.6875rem;
+    margin: 0 auto;
+
+    .flex{
+    display: flex;
+    gap: 0.5625rem;
+  }
+`
