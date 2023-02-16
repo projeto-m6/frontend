7422a7d76cd6b376c7170e1f6ed710c8ffe54ceb
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-15 22:19:32 -0400

    feat: AuctionCards Component

--- src/components/AuctionCards/index.tsx
+++ src/components/AuctionCards/index.tsx
@@ -1,59 +1,59 @@
-import { IAnnouncement } from "../../mocks/auto-vehicles"
-import { AuctionArticle, AuctionTime, AuctionText, AuctionFooter, AuctionImage } from "./style"
-import {BsClock} from "react-icons/bs"
-import { Avatar } from "../Avatar"
+import { IAnnouncement } from '../../mocks/auto-vehicles';
+import {
+  AuctionArticle,
+  AuctionTime,
+  AuctionText,
+  AuctionFooter,
+  AuctionImage,
+  AuctionFooterAdvertiser,
+} from './style';
+import { BsClock, BsArrowRight } from 'react-icons/bs';
+import { Avatar } from '../Avatar';
+import { B1400, B2500, H6600, H7500 } from '../../styles/typography';
 
-interface IAuctionCardsProps{
-    announcement: IAnnouncement
+interface IAuctionCardsProps {
+  announcement: IAnnouncement;
 }
 
-export const AuctionCards = ({announcement}: IAuctionCardsProps) =>{
+export const AuctionCards = ({ announcement }: IAuctionCardsProps) => {
+  return (
+    <AuctionArticle>
+      <AuctionImage>
+        <img src={announcement.image[0]} alt="car" />
+        <AuctionText>
+          <AuctionTime>
+            <BsClock />
+            <H7500>01:58:00</H7500>
+          </AuctionTime>
+          <H6600>Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200 </H6600>
 
-    return(
+          <B1400>{announcement.description}</B1400>
 
-        <AuctionArticle>
+          <div className="avatar">
+            <Avatar username={announcement.user.name} />
+            <B2500>{announcement.user.name}</B2500>
+          </div>
 
-                <AuctionImage>
-                <img src={announcement.image[0]} alt="car" />
-                </AuctionImage>
-            <AuctionText>
-
-                
-                    <AuctionTime>
-                        <BsClock/>
-                        <p>TEMP</p>
-                    </AuctionTime>
-                        <div>
-                            <h2>Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200</h2>
-                        </div>
-
-                        <div>
-                            <p>{announcement.description}</p>
-                        </div>
-
-                        <div className="avatar">
-                            <Avatar username ={announcement.user.name}/>
-                            <p>{announcement.user.name}</p>
-                        </div>
-
-                        <div className="price">
-                            <div>
-                                <p>{announcement.year}</p>
-                                <p>{announcement.year}</p>
-                            </div>
-                            <p>{announcement.price}</p>
-                        </div>
-
-                    </AuctionText>
-                
-
-            <AuctionFooter>
-                <p>Acessar pagina de leilão</p>
-                <p>Icone</p>
-            </AuctionFooter>
-
-        </AuctionArticle>
-
-
-    )
-}
+          <div className="price">
+            <div>
+              <p>{announcement.year}</p>
+              <p>{announcement.mileage}</p>
+            </div>
+            <H7500>{announcement.price}</H7500>
+          </div>
+        </AuctionText>
+      </AuctionImage>
+      {true ? (
+        <AuctionFooter>
+          <p>Acessar pagina de leilão</p>
+          <BsArrowRight />
+        </AuctionFooter>
+      ) : (
+        <AuctionFooterAdvertiser>
+          <button>Editar</button>
+          <button>Ver Como</button>
+        </AuctionFooterAdvertiser>
+      )}
+    </AuctionArticle>
+  );
+};
