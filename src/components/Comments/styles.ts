52ec54c8b1e17d3d790a1433c509f03a4e37d4aa
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-22 22:48:02 -0400

    feat: comments component

---
+++ src/components/Comments/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .comments {
+    background-color: ${({ theme }) => theme.grey10};
+    padding: 2.25rem 2.75rem;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+
+    ul {
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      gap: 44px;
+
+      li {
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+        div {
+          display: flex;
+          gap: 0.5rem;
+          align-items: baseline;
+        }
+      }
+    }
+
+    .time {
+      color: ${({ theme }) => theme.grey3};
+      font-family: 'Inter';
+      font-size: 0.75rem;
+      font-weight: 400;
+    }
+  }
+`;
