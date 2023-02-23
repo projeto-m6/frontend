54680bd8288109b766c192e29a93e4db0b5db184
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-23 10:46:04 -0400

    feat: update component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,17 +1,19 @@
-import { Container } from "./style";
+import { Container } from './style';
+import logo from '../../assets/Motors_shop2.png';
+import arrow from '../../assets/angle-up.png';
 
 const Footer = () => {
   const handleScrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
     <Container>
       <div>
-        <img id="logotipo" src={"Motors shop.png"} alt="" />
+        <img id="logotipo" src={logo} alt="" />
         <p>© 2022 - Todos os direitos reservados.</p>
         <button onClick={handleScrollToTop}>
-          <img src="angle-up.png" alt="" />
+          <img src={arrow} alt="" />
         </button>
       </div>
     </Container>
