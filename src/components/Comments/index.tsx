52d3c51792c0408d0a0afb92485242591487e008
Rodrigo Nunes <imrodrigon@gmail.com>
2023-02-27 13:57:39 -0400

    feat: view publication date

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,37 +1,61 @@
+import { Announcement } from '../../contexts/announcement';
 import { B2400, B2500, H6600 } from '../../styles/typography';
 import { Avatar } from '../Avatar';
 import { Container } from './styles';
 
-export const Comments = () => {
+interface CommentsProps {
+  announcement: Announcement | null;
+}
+
+export const Comments = ({ announcement }: CommentsProps) => {
+  function monthDiff(dateFrom: Date, dateTo: Date) {
+    return (
+      dateTo.getMonth() - dateFrom.getMonth() + 12 * (dateTo.getFullYear() - dateFrom.getFullYear())
+    );
+  }
+
+  const publishingTime = (date: string) => {
+    const now = new Date();
+    const publicationDate = new Date(date);
+    const timeInMilliseconds = Math.abs(publicationDate.getTime() - now.getTime());
+    const diffSec = Math.ceil(timeInMilliseconds / 1000);
+    const diffMin = Math.ceil(timeInMilliseconds / (1000 * 60));
+    const diffHour = Math.ceil(timeInMilliseconds / (1000 * 60 * 60));
+    const diffDays = Math.ceil(timeInMilliseconds / (1000 * 60 * 60 * 24));
+    const diffMonths = Math.abs(monthDiff(publicationDate, now));
+    const diffYear = Math.abs(publicationDate.getFullYear() - now.getFullYear());
+
+    if (diffSec < 60) {
+      return 'agora';
+    } else if (diffMin < 60) {
+      return `há ${diffMin} ${diffMin === 1 ? 'minuto' : 'minutos'} atrás`;
+    } else if (diffHour < 24) {
+      return `há ${diffHour} ${diffHour === 1 ? 'hora' : 'horas'} atrás`;
+    } else if (diffMonths < 1) {
+      return `há ${diffDays} ${diffDays === 1 ? 'dia' : 'dias'} atrás`;
+    } else if (diffYear < 1) {
+      return `há ${diffMonths} ${diffMonths === 1 ? 'mês' : 'meses'} atrás`;
+    } else if (diffYear >= 1) {
+      return `há ${diffYear} ${diffYear === 1 ? 'ano' : 'anos'} atrás`;
+    }
+  };
+
   return (
     <Container>
       <div className="comments">
         <H6600>Comentários</H6600>
         <ul>
-          <li>
-            <div>
-              <Avatar username="Rodrigo Nunes" />
-              <B2500>Rodrigo Nunes</B2500>
-              <span className="time">há 3 dias</span>
-            </div>
-            <B2400>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-              has been the industry's standard dummy text ever since the 1500s, when an unknown
-              printer took a galley of type and scrambled it to make a type specimen book.
-            </B2400>
-          </li>
-          <li>
-            <div>
-              <Avatar username="Rodrigo Nunes" />
-              <B2500>Rodrigo Nunes</B2500>
-              <span className="time">há 3 dias</span>
-            </div>
-            <B2400>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-              has been the industry's standard dummy text ever since the 1500s, when an unknown
-              printer took a galley of type and scrambled it to make a type specimen book.
-            </B2400>
-          </li>
+          {announcement &&
+            announcement.comments.map((comment) => (
+              <li key={comment.id}>
+                <div>
+                  <Avatar username={comment.user.name} />
+                  <B2500>{comment.user.name}</B2500>
+                  <span className="time">{publishingTime(comment.created_at)}</span>
+                </div>
+                <B2400>{comment.comment}</B2400>
+              </li>
+            ))}
         </ul>
       </div>
     </Container>
